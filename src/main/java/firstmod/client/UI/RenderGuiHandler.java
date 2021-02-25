2c1cb7413c54cadc3561977fc887d160e39f305e
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2021-02-25 04:03:09 +0100

    Improvement

--- src/main/java/firstmod/client/UI/RenderGuiHandler.java
+++ src/main/java/firstmod/client/UI/RenderGuiHandler.java
@@ -10,29 +10,15 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
 @EventBusSubscriber(modid = "firstmod_123",bus = Bus.FORGE)
 public class RenderGuiHandler {
-		
-	  
+
 		@SubscribeEvent
-		public static void textRenderGui(RenderGameOverlayEvent.Post event) 
-		{
-			 int Colors;
-			if(MagicManager.Mana <= 10)
-			{
-				Colors = 16321548;
-			}else {
-				Colors = 799993;
-			}
-			
-			 if(event.getType() == RenderGameOverlayEvent.ElementType.TEXT)
-		     {
-				 
-				    MatrixStack stack = event.getMatrixStack();
-				    FontRenderer font = Minecraft.getInstance().fontRenderer;
-				    font.func_243248_b(stack, new StringTextComponent("Mana: "+MagicManager.Mana), 12, 12,Colors);
-				 
-		     }
+		public static void textRenderGui(RenderGameOverlayEvent.Post event) {
+			if(event.getType() == RenderGameOverlayEvent.ElementType.TEXT) {
+		        Minecraft.getInstance().fontRenderer.func_243248_b(event.getMatrixStack(), new StringTextComponent("Mana: "+ MagicManager.Mana ), 12, 12, MagicManager.Mana <=10 ? 16321548 : 799993);
+		        }
 		}
 	
 }
