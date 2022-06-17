3e39ae7c5caea5d582164c0191405a95c6aa1f62
GunMetalBlack <powerhousegames206@gmail.com>
2022-06-17 00:20:01 -0400

    Working Good Fun with flux food

--- src/main/java/phlaxmod/common/item/ItemPhlaxFlux.java
+++ src/main/java/phlaxmod/common/item/ItemPhlaxFlux.java
@@ -1,6 +1,11 @@
 package phlaxmod.common.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import phlaxmod.common.capability.PhlaxModCapabilities;
+import phlaxmod.common.capability.phlaxplayerdataholder.IPhlaxPlayerDataHolderCapability;
 
 public class ItemPhlaxFlux extends Item {
 
@@ -8,12 +13,15 @@ public class ItemPhlaxFlux extends Item {
 		super(properties);
 	}
 	
-//TODO REWRITE FOR CAPABILITY
-//	@Override
-//		public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
-//			MagicManager.MagicController();
-//			MagicManager.RegenMana = true;
-//			return super.onItemUseFinish(stack, worldIn, entityLiving);
-//		}
+
+	@Override
+		public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+			IPhlaxPlayerDataHolderCapability playerData = entityLiving.getCapability(PhlaxModCapabilities.PLAYER_DATA_HOLDER_CAPABILITY).orElse(null);
+			if(playerData != null){
+				playerData.setShouldRegenMana(true);
+				playerData.setManaRegenRate(10);
+			}
+			return super.onItemUseFinish(stack, worldIn, entityLiving);
+		}
 
 }
